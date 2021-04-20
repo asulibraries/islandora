78ae63c872bd61504ce8e3835c8d2231f79c537c
elizoller <zoller.eli@gmail.com>
2021-04-20 21:48:11 +0000

    pulling the text track outside of the audio element so its not hidden

---
+++ modules/islandora_audio/js/audio.js
@@ -0,0 +1,45 @@
+/*jslint browser: true*/
+/*global Audio, Drupal*/
+/**
+ * @file
+ * Displays Audio viewer.
+ */
+(function ($, Drupal) {
+    'use strict';
+
+    /**
+     * If initialized.
+     * @type {boolean}
+     */
+    var initialized;
+    /**
+     * Unique HTML id.
+     * @type {string}
+     */
+    var base;
+
+    function init(context,settings){
+        if (!initialized){
+            console.log("hello audio");
+            initialized = true;
+	    $('audio')[0].textTracks[0].oncuechange = function() {
+		console.log("changed");
+		var currentCue = this.activeCues[0].text;
+		$('#audioTrack').html(currentCue);
+	    }
+        }
+    }
+    Drupal.Audio = Drupal.Audio || {};
+
+    /**
+     * Initialize the Audio Viewer.
+     */
+    Drupal.behaviors.Audio = {
+        attach: function (context, settings) {
+            init(context,settings);
+        },
+        detach: function () {
+        }
+    };
+
+})(jQuery, Drupal);
